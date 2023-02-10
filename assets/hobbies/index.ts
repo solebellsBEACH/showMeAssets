175187d20a0f53829901c70943f6c5ad3767e41d
Lucas Xavier <lucassxxavier@gmail.com>
2023-02-10 10:14:49 -0300

    adding assets

---
+++ assets/hobbies/index.ts
@@ -0,0 +1,9 @@
+import guitarra from './guitarra.jpg';
+import skate from './skate.jpg';
+import surf from './surf.jpg';
+
+export const hobbiesAssets = {
+  surf,
+  guitarra,
+  skate,
+};
---
+++ assets/stacks/index.ts
@@ -0,0 +1,19 @@
+import adonisjs from './adonis.svg';
+import express from './express.svg';
+import nestjs from './nestjs.svg';
+import nextjs from './nextjs.svg';
+import nodejs from './nodejs.svg';
+import reactjs from './react.svg';
+import reactnative from './reactnative.svg';
+import typescript from './typescript.svg';
+
+export const stackAssets = {
+  adonisjs,
+  express,
+  nestjs,
+  nextjs,
+  nodejs,
+  reactjs,
+  reactnative,
+  typescript,
+};
